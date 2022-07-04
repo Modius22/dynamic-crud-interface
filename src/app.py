c93e49a97aca1c0420176b62d71d80022b7fdcf2
Christian Piazzi <christian.piazzi@heraeus.com>
2022-07-04 16:28:33 +0200

    change to FastAPI

--- src/app.py
+++ src/app.py
@@ -1,37 +1,42 @@
 import json
+import fastapi
 
-from flask import Flask
-from flask_cors import CORS, cross_origin
+#from flask import Flask
+#from flask_cors import CORS, cross_origin
 
-import helpers.db as db
-
-app = Flask(__name__)
-
-cors = CORS(app)
-app.config["CORS_HEADERS"] = "Content-Type"
+import uvicorn
+from fastapi import FastAPI
 
 
-@app.route("/")
-@cross_origin()
+#import helpers.db as db
+
+app = FastAPI()
+
+#cors = CORS(app)
+#app.config["CORS_HEADERS"] = "Content-Type"
+
+
+@app.get("/")
+#@cross_origin()
 def hello_world():  # put application's code here
     return "Hello World!"
 
 
-@app.route("/get_db_names")
-@cross_origin()
-def get_db_data():
-    test = db.DB()
-    data = test.get_table_names()
-    return json.dumps(data)
+#@app.get("/get_db_names")
+#@cross_origin()
+#def get_db_data():
+#    test = db.DB()
+#    data = test.get_table_names()
+#    return json.dumps(data)
 
 
-@app.route("/<table>/get_table_structure")
-@cross_origin()
-def get_table_structure(table):
-    test = db.DB()
-    data = test.get_table_columns(table)
-    return data
+#@app.get("/<table>/get_table_structure")
+#@cross_origin()
+#def get_table_structure(table):
+#    test = db.DB()
+#    data = test.get_table_columns(table)
+#    return data
 
 
 if __name__ == "__main__":
-    app.run()
+    uvicorn.run(app,host='0.0.0.0', port=2000)
